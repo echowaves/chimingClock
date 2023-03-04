8a5ed0b15898d70f29df82e6b980ad2d779f72a1
dmitryame <dmitry@custommode.com>
2023-03-03 22:16:48 -0500

    wip

--- App.js
+++ App.js
@@ -5,6 +5,26 @@ import { Audio } from "expo-av"
 
 import dayjs from "dayjs"
 
+const hoursSounds = {
+  1: require("./assets/hour1.m4a"),
+  2: require("./assets/hour2.m4a"),
+  3: require("./assets/hour3.m4a"),
+  4: require("./assets/hour4.m4a"),
+  5: require("./assets/hour5.m4a"),
+  6: require("./assets/hour6.m4a"),
+  7: require("./assets/hour7.m4a"),
+  8: require("./assets/hour8.m4a"),
+  9: require("./assets/hour9.m4a"),
+  10: require("./assets/hour10.m4a"),
+  11: require("./assets/hour11.m4a"),
+  12: require("./assets/hour12.m4a"),
+}
+const minutesSounds = {
+  15: require("./assets/minute15.m4a"),
+  30: require("./assets/minute30.m4a"),
+  45: require("./assets/minute45.m4a"),
+}
+
 export default function App() {
   useKeepAwake()
   const { height, width, scale, fontScale } = useWindowDimensions()
@@ -48,25 +68,11 @@ export default function App() {
   const play = async (minute) => {
     if (minute === 0) {
       const hour = dayjs(currentTime).hour()
-      const hourAsset = switch(minute) {
-        case 15:
-          return require(`./assets/minute15.mp3`)
-case 30:
-  return require(`./assets/minute30.mp3`)
-  case 45:
-    return require(`./assets/minute45.mp3`)
-    
-      }
-
-      const minuteAsset = `./assets/hour${minute}.mp3`
-      const { sound } = await Audio.Sound.createAsync(require(hourString))
-
+      const { sound } = await Audio.Sound.createAsync(hoursSounds[hour])
       await sound.playAsync()
       await sound.unloadAsync()
     } else {
-      const { sound } = await Audio.Sound.createAsync(
-        require(`./assets/minute${minute}.mp3`),
-      )
+      const { sound } = await Audio.Sound.createAsync(minutesSounds[minute])
       await sound.playAsync()
       await sound.unloadAsync()
     }
